4a217de48195ecc6b0466d425fe9ce8961589870
olivia <opierez@gmail.com>
2023-02-16 14:15:17 -0500

    code cleanup

--- client/src/components/AddExpenseForm.js
+++ client/src/components/AddExpenseForm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Select from 'react-select';
 
 function AddExpenseForm({ user, updateExpenses, updateAmountSpent, selectedExpense, handleSelectedExpense, handleShowForm }) {
 
